48a60d599deda1738924627de7b09234b5b9cd76
xash4492 <119355463+xash4492@users.noreply.github.com>
2023-02-14 20:03:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-window.onload = function () {
-    const menuBtn = document.querySelector(".hamburger");
-    const mobileMenu = document.querySelector(".mobile-nav")
-
-    menuBtn.addEventListener('click', function () {
-        menuBtn.classList.toggle("is-active");
-        mobileMenu.classList.toggle("is-active");
-    })
-}
+AOS.init()
